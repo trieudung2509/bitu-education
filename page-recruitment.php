9bf15053c4c5c0fb6a7e2655455143a6ac4c6a9d
Frank <frank.phan@helius-software.com>
2022-04-15 22:47:32 +0700

    feat: UI page recruit

---
+++ page-recruitment.php
@@ -0,0 +1,407 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+
+  <div class="page-recruitment">
+    <!-- block-recruitment -->
+    <div class="block-recruitment-1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 col-12">
+            <div class="block1-recruitment-left">
+              <h5>Nghĩ lớn Làm nhỏ Tham vọng lớn</h5>
+              <p>nhưng hãy bắt đầu bằng cách cố gắng làm những việc nhỏ bé theo cách tuyệt vời nhất</p>
+              <button type="button" class="btn btn-primary btn-contact">Gia nhập với chúng tôi</button>
+            </div>
+          </div>
+          <div class="col-md-7 col-12">
+            <div class="block1-recruitment-right">
+              <img src="/images/Mask group.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- the end block recruitment -->
+    <!-- block mission -->
+    <div class="block-mission">
+      <div class="container">
+        <div class="row">
+          <div class="mission-title">
+            <h2>Sứ mệnh</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-5 col-12">
+            <div class="block-mission-left">
+              <ul class="ul-mission">
+                <li class="li-mission">
+                  <div class="item-mission">
+                    <img src="/images/mission-icon1.jpg" alt="">
+                    <p>
+                      Giúp học viên ở các nước không nói tiếng Anh có thể cải thiện tốt nhất khả năng giao tiếp bằng ngôn ngữ này
+                    </p>
+                  </div>
+                </li>
+                <li class="li-mission">
+                  <div class="item-mission">
+                    <img src="/images/mission-icon2.jpg" alt="">
+                    <p>
+                      Tạo ra một nền tảng kết nối, một MXH “kiểu mới”- nơi học viên sử dụng tiếng Anh như công cụ chính để giao lưu, kết bạn, trao đổi văn hóa
+                    </p>
+                  </div>
+                </li>
+                <li class="li-mission">
+                  <div class="item-mission">
+                    <img src="/images/mission-icon3.jpg" alt="">
+                    <p>
+                      Phổ cập tiếng Anh cho mọi lứa tuổi, mọi tầng lớp, từ thành thị đến nông thôn, từ người nghèo cho đến người giàu đều có thể tham gia Bitu
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-7 col-12">
+            <div class="mission-right">
+              <h2>
+                Giúp mọi người
+                <span>nói tiếng Anh tốt hơn</span>
+                bằng cách tập trung vào thực hành
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- the end block mission -->
+    <div class="block-suitable">
+      <div class="container">
+        <div class="row">
+          <div class="mission-title">
+            <h2>Ai phù hợp với Bitu ?</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-7 col-12">
+            <div class="suitable-left">
+              <button type="button" class="btn btn-primary btn-ambitious">Có tham vọng</button>
+              <button type="button" class="btn btn-primary btn-experiment">Sẵn sàng thử nghiệm những thứ mới mẻ</button>
+              <button type="button" class="btn btn-primary btn-handle">Thích giải quyết vấn đề</button>
+              <p>
+                Bitu được xây dựng dựa trên văn hóa làm việc chăm chỉ, luôn đổi mới và sẵn sàng chấp nhận những thứ mới mẻ. Chúng tôi luôn đón chào những ứng viên có tham vọng, sẵn sàng làm mọi thứ để thành công. Chúng tôi không quan tâm bạn đến từ đâu, trông bạn thế nào hoặc quen biết những ai - Bitu tìm những ứng viên phù hợp nhất với công việc và tạo ra môi trường hỗ trợ nhân viên, giúp họ cảm thấy hào hứng với công việc
+              </p>
+            </div>
+          </div>
+          <div class="col-md-5 col-12">
+            <div class="suitable-left">
+              <img src="images/suitable-img.jpg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- block: block-recruit -->
+    <div class="block-recruit">
+      <div class="container">
+        <div class="row">
+          <div class="mission-title">
+            <h2>Ai phù hợp với Bitu ?</h2>
+            <h6>Hãy cùng chúng tôi xây dựng những sản phẩm tuyệt vời</h6>
+          </div>
+        </div>
+        <div class="recruit-top">
+          <div class="row">
+            <div class="mission-title">
+              <h2>Marketing</h2>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Product Manager, Integrations</h4>
+                <p>Thương lượng</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Group Product Manager, Permissions & Federation</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Head of Product Design</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Account Executive - Majors</h4>
+                <p>Up to $3k</p>
+                <a type="button" class="btn btn-primary btn-apply">Apply</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Business Development Representative</h4>
+                <p>Up to $3k</p>
+                <a type="button" class="btn btn-primary btn-apply">Apply</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Group Product Manager, Permissions & Federation</h4>
+                <p>Thương lượng</p>
+                <a type="button" class="btn btn-primary btn-apply">Apply</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Head of Product Design</h4>
+                <p>Up to $3k</p>
+                <a type="button" class="btn btn-primary btn-apply">Apply</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="recruit-top">
+          <div class="row">
+            <div class="mission-title">
+              <h2>IT & Design</h2>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Product Manager, Integrations</h4>
+                <p>Thương lượng</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Group Product Manager, Permissions & Federation</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Head of Product Design</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Account Executive - Majors</h4>
+                <p>Up to $3k</p>
+                <a type="button" class="btn btn-primary btn-apply">Apply</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="recruit-top">
+          <div class="row">
+            <div class="mission-title">
+              <h2>Sale</h2>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Product Manager, Integrations</h4>
+                <p>Thương lượng</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Group Product Manager, Permissions & Federation</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="item-recruit">
+                <h4>Head of Product Design</h4>
+                <p>15.000.000-25.000.000</p>
+                <button type="button" class="btn btn-primary btn-apply">Apply</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- block: block-interest -->
+    <div class="block-interest">
+      <div class="container">
+          <div class="row">
+            <div class="mission-title">
+                <h2>Quyền lợi</h2>
+            </div>
+          </div>
+          <div class="row">
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/computer.svg" />
+                    <h5>Máy tính</h5>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/interest-2.svg" />
+                    <h5>Bữa trưa</h5>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/interest-3.svg" />
+                    <h5>Bảo hiểm</h5>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/interest-4.svg" />
+                    <h5>Thưởng những ngày lễ lớn</h5>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/interest-5.svg" />
+                    <h5>Cafe & Trà miễn phí</h5>
+                  </div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item-interest">
+                    <img src="images/interest-6.svg" />
+                    <h5>Du lịch, team building</h5>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+
+    <!-- block : block-progress -->
+    <div class="block-progress">
+        <div class="block-container-progress">
+          <div class="container">
+            <div class="row">
+              <div class="mission-title">
+                <h2>Quy trình tuyển dụng</h2>
+              </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>01</h4>
+                        <h5>Xét duyệt hồ sơ</h5>
+                        <p>
+                          Đảm bảo kinh nghiệm của bạn phù hợp với công ty
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>02</h4>
+                        <h5>Phỏng vấn sơ tuyển</h5>
+                        <p>
+                          Chúng tôi muốn làm quen với bạn
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>03</h4>
+                        <h5>Mini Project</h5>
+                        <p>
+                          Một bài tập nhỏ  cho vị trí mà bạn ứng tuyển
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>04</h4>
+                        <h5>Phỏng vấn</h5>
+                        <p>
+                          Phỏng vấn với Team Lead
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>05</h4>
+                        <h5>Offer</h5>
+                        <p>
+                          Bitu gửi Offer đến ứng viên
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-2 item-progress">
+                    <div class="block-item-step">
+                        <h4>06</h4>
+                        <h5>Career Launch</h5>
+                        <p>
+                          Chúc mừng, bạn là một thành viên trong gia đình Bitu
+                        </p>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+  </div>
+
+
+
+  <?php include "templates/footer.php"; ?>
+
+  <script>
+    $(function() {
+      // ======================> benefit <======================
+      // $(function() {
+      // 	var benafitSlider= $('.con-benefit .box-slider').slick({
+      // 		speed: 600,
+      // 		autoplaySpeed: 6000,
+      // 		autoplay: true,
+      // 		infinite: true,
+      // 		swipe: true,
+      // 		fade: false,
+      // 		dots: false,
+      // 		arrows: false,
+      // 		slidesToShow: 3,
+      // 		slidesToScroll: 1,
+      // 		responsive: [
+      // 			{
+      // 				breakpoint: 992,
+      // 				settings: {
+      // 					slidesToShow: 2
+      // 				}
+      // 			},
+      // 			{
+      // 				breakpoint: 768,
+      // 				settings: {
+      // 					slidesToShow: 1
+      // 				}
+      // 			}
+      // 		]
+      // 	});
+
+      // 	$('.con-benefit .slick-prev').on('click', function() {
+      // 		benafitSlider.slick('prev');
+      // 	});
+      // 	$('.con-benefit .slick-next').on('click', function() {
+      // 		benafitSlider.slick('next');
+      // 	});
+      // });
+    });
+  </script>
+
+</body>
+
+</html>
