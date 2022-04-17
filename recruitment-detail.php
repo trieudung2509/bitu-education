23534737c9a0a440d9162f4c65382ecde56a42d5
Frank <frank.phan@helius-software.com>
2022-04-17 16:43:42 +0700

    feat(recruitment): UI desktop + mobile

---
+++ recruitment-detail.php
@@ -0,0 +1,92 @@
+<?php include "templates/head.php"; ?>
+<body>
+    <?php include "templates/header.php"; ?>
+    <div class="recruiment-detail">
+        <div class="block-recruiment-top">
+            <h2>Motion Graphic Animator</h2>
+          <div class="description-top">
+            <img src="images/time-clock.svg" />
+            <span class="time-des">Full-Time</span>
+          </div>
+        </div>
+        <div class="list-recruiment">
+            <div class="container">
+              <div class="row">
+                  <div class="block-item-recruitment">
+                      <h2 class="title-recruiment">Responsibilities</h2>
+                      <ul class="ul-rules">
+                        <li class="item-rules">
+                          <p>Working experience of CSS frameworks (Bootstrap / Foundation / Material, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding web dev tools (Gulp / WebPack / Less / Sass, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding one of the following technologies: TypeScript, RxJS, D3JS, Elm, React Native, Xamarin, Cordova …</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Know more about other languages like Javascript: PHP, Java, C # …</p>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="block-item-recruitment">
+                      <h2 class="title-recruiment">Requirements</h2>
+                      <ul class="ul-rules">
+                        <li class="item-rules">
+                          <p>Working experience of CSS frameworks (Bootstrap / Foundation / Material, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding web dev tools (Gulp / WebPack / Less / Sass, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding one of the following technologies: TypeScript, RxJS, D3JS, Elm, React Native, Xamarin, Cordova …</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Know more about other languages like Javascript: PHP, Java, C # …</p>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="block-item-recruitment">
+                      <h2 class="title-recruiment">Offer</h2>
+                      <ul class="ul-rules">
+                        <li class="item-rules">
+                          <p>Working experience of CSS frameworks (Bootstrap / Foundation / Material, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding web dev tools (Gulp / WebPack / Less / Sass, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding one of the following technologies: TypeScript, RxJS, D3JS, Elm, React Native, Xamarin, Cordova …</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Know more about other languages like Javascript: PHP, Java, C # …</p>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="block-item-recruitment">
+                      <h2 class="title-recruiment">About us</h2>
+                      <ul class="ul-rules">
+                        <li class="item-rules">
+                          <p>Working experience of CSS frameworks (Bootstrap / Foundation / Material, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding web dev tools (Gulp / WebPack / Less / Sass, …)</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Understanding one of the following technologies: TypeScript, RxJS, D3JS, Elm, React Native, Xamarin, Cordova …</p>
+                        </li>
+                        <li class="item-rules">
+                          <p>Know more about other languages like Javascript: PHP, Java, C # …</p>
+                        </li>
+                      </ul>
+                  </div>
+                  <div class="block-button">
+                    <button type="submit" class="btn btn-apply"><span>Apply to Position</span></button>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </div>
+    <?php include "templates/footer.php"; ?>
+</body>
+</html>
