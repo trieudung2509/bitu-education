f65e6787211a6c6c300a8bd4b726da6d2cbb6805
TinhNN <nguyennhutinh18@gmail.com>
2022-04-17 23:22:05 +0700

    Fix home

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer">
-	<div class="container containe-lg">
+	<div class="container">
 		<ul class="footer-nav nav justify-content-center">
 			<li class="nav-item"><a class="nav-link" href="#">Về chúng tôi</a></li>
 			<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
@@ -12,37 +12,118 @@
 
 		<div class="footer-main row">
 			<div class="col-12 col-md-2">
-				<a href="#" class="footer-logo">
-					<img src="images/footer_logo.svg" alt="Bitu">
+				<a href="#" class="footer-logo over">
+					<img src="images/logo_white.svg" alt="Bitu" class="d-none d-md-inline-block">
+					<img src="images/logo_sp_white.png" alt="Bitu" class="d-inline-block d-md-none">
 				</a>
 			</div>
+
 			<div class="col-12 col-md-10">
-				<div class="footer-contact row">
-					<div class="footer-contact__address col-md-9">
-						<p><i class="fas fa-map-marker-alt"></i>Địa chỉ: Tầng 10, Tháp D, Tòa Gold Tower, số 275 Nguyễn Trãi, Thanh Xuân Trung, Thanh Xuân, Hà Nội.</p>
-						<p><i class="fas fa-envelope"></i>Email: support@giaotieptienganh.com.vn</p>
-						<p><i class="fas fa-phone-alt"></i>SĐT: 0981115411 - 0904565411</p>
+				<div class="footer-contact">
+					<div class="footer-contact__address row">
+						<div class="col-md-3">
+							<p class="item-icon-text">
+								<i class="fas fa-map-marker-alt ic"></i>
+								<span class="text">
+									Vietnam
+									<br />
+									<a href="https://www.google.com/maps/place/Gold+Tower+275+Nguy%E1%BB%85n+Tr%C3%A3i/@20.9935547,105.8048872,17z/data=!4m9!1m2!2m1!1zVMOyYSBHb2xkIFRvd2VyLCBz4buRIDI3NSBOZ3V54buFbiBUcsOjaSwgVGhhbmggWHXDom4gVHJ1bmcsIFRoYW5oIFh1w6JuLCBIw6AgTuG7mWku!3m5!1s0x3135ad72e2a9056f:0xfe1128ecfe0feebc!8m2!3d20.9935807!4d105.8069061!15sClRUw7JhIEdvbGQgVG93ZXIsIHPhu5EgMjc1IE5ndXnhu4VuIFRyw6NpLCBUaGFuaCBYdcOibiBUcnVuZywgVGhhbmggWHXDom4sIEjDoCBO4buZaS5aUSJPdMOyYSBnb2xkIHRvd2VyIHPhu5EgMjc1IG5ndXnhu4VuIHRyw6NpIHRoYW5oIHh1w6JuIHRydW5nIHRoYW5oIHh1w6JuIGjDoCBu4buZaZIBBWhvdGVsmgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVU00WDNSaU5YRjNSUkFC" target="_blank">
+										Tầng 10, Tháp D, Tòa Gold Tower, số 275 Nguyễn Trãi, Thanh Xuân Trung, Thanh Xuân, Hà Nội.
+									</a>
+								</span>
+							</p>
+						</div>
+
+						<div class="col-md-3">
+							<p class="item-icon-text">
+								<i class="fas fa-map-marker-alt ic"></i>
+								<span class="text">
+									Thailand
+									<br />
+									<a href="https://www.google.com/maps/place/Phairojkijja+Tower/@13.6677848,100.6388821,17z/data=!3m1!4b1!4m5!3m4!1s0x311d5fe9f44a6049:0x934f5041e8488b3f!8m2!3d13.6677847!4d100.6410566" target="_blank">
+										825 Phairojkijja Building, Thanon Debaratana, Bang Na Nuea, Bang Na, Bangkok, 10260.
+									</a>
+								</span>
+							</p>
+						</div>
+
+						<div class="col-md-3">
+							<p class="item-icon-text">
+								<i class="fas fa-map-marker-alt ic"></i>
+								<span class="text">
+									Singapore
+									<br />
+									<a href="https://www.google.com/maps/place/68+Circular+Rd,+Unit+02-01,+Singapore+049422/@1.2864472,103.8469536,17z/data=!3m1!4b1!4m5!3m4!1s0x31da190bb5febd0b:0x3999e3ace9eb9af!8m2!3d1.2864418!4d103.8491423" target="_blank">
+										68 CIRCULAR ROAD #02-01 SINGAPORE
+									</a>
+								</span>
+							</p>
+						</div>
+
+						<div class="col-md-3">
+							<p class="item-icon-text">
+								<i class="fas fa-map-marker-alt ic"></i>
+								<span class="text">
+									Philippines
+									<br />
+									<a href="https://www.google.com/maps/place/Atlanta+Center+Building,+31+Annapolis,+San+Juan,+Metro+Manila,+Philippines/@14.6047525,121.0508948,17z/data=!3m1!4b1!4m5!3m4!1s0x3397b7de9af441e5:0x6d92bd330eba0e11!8m2!3d14.6047473!4d121.0530835" target="_blank">
+										Atlanta Center, 31 Annapolis Street, Greenhills, San Juan City, Metro Manila, Philippines.
+									</a>
+								</span>
+							</p>
+						</div>
 					</div>
-					<div class="footer-contact__social col-md-3">
-						<p><span>Connect with Bitu</span><i class="fab fa-facebook-f"></i></p>
-					</div>
+
 				</div>
 			</div>
 
 			<div class="footer-bottom">
 				<div class="row justify-content-center justify-content-md-end align-items-md-end">
-					<div class="col-md-5 footer-copyright">
-						<p>Copyright @ 2021 Bitu. All rights reserved</p>
+					<div class="col-md-5">
+						<div class="footer-contact__email px-2 px-md-0">
+							<p class="item-icon-text">
+								<i class="fas fa-envelope ic"></i>
+								<span class="text">
+									<a href="mailTo:support@giaotieptienganh.com.vn">Email: support@giaotieptienganh.com.vn</a>
+								</span>
+							</p>
+							<p class="item-icon-text">
+								<i class="fas fa-phone-alt ic"></i>
+								<span class="text">
+									SĐT: <a href="tel:0981115411">0981115411</a> - <a href="tel:0904565411">0904565411</a>
+								</span>
+							</p>
+						</div>
+						<div class="footer-contact__social">
+							<p>
+								<a href="https://www.facebook.com/BituEnglish" target="_blank" class="item-connect-fb d-inline-flex align-items-center">
+									<i class="fab fa-facebook-f d-inline d-md-none"></i>
+									<span>Connect with Bitu</span>
+									<i class="fab fa-facebook-f d-none d-md-inline"></i>
+								</a>
+							</p>
+						</div>
+						<div class="footer-copyright d-none d-md-block">
+							<p>Copyright @ 2021 Bitu. All rights reserved</p>
+						</div>
 					</div>
-					<div class="col-md-5 footer-dowload">
-					<ul class="nav justify-content-md-end">
-						<li class="nav-item">
-							<a class="nav-link active" href="#"><img src="images/img_dowload_ios.png" alt="Dowload Apple Store"></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#"><img src="images/img_dowload_android.png" alt="Dowload Google Play"></a>
-						</li>
-					</ul>
+
+					<div class="col-md-5 footer-download-wrapper px-0">
+						<div class="footer-download">
+							<p class="text-center">Download app Bitu</p>
+							<ul class="row g-0 align-items-center">
+								<li class="col-6">
+									<a class="over" href="#"><img src="images/img_download_ios.png" alt="download Apple Store" class="w-100"></a>
+								</li>
+								<li class="col-6">
+									<a class="over" href="#"><img src="images/img_download_android.png" alt="download Google Play" class="w-100"></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="footer-copyright d-block d-md-none">
+						<p>Copyright @ 2021 Bitu. All rights reserved</p>
 					</div>
 				</div>
 			</div>
--- templates/head.php
+++ templates/head.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 	<head>
 		<meta charset="UTF-8">
 		<title>Bitu Education</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="">
+		<link data-n-head="ssr" rel="icon" type="image/x-icon" href="images/favicon.ico">
 
 		<!-- STYLESHEET -->
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
