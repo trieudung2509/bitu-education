25c7280bf56f9d0f5ed500dd5c84742f6572d899
khoa96 <khoanguyenptit96@gmail.com>
2022-04-20 08:36:42 +0700

    Task: ui blog detail

--- templates/head.php
+++ templates/head.php
@@ -16,6 +16,8 @@
 
 		<link rel="stylesheet" type="text/css" href="css/blog.css">
 
+		<link rel="stylesheet" type="text/css" href="css/blog-detail.css">
+
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file://-->
 		<!--if lt IE 9
