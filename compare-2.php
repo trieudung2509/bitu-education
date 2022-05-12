64866229d49029191988146be5e80ad516c33a35
TinhNN <nguyennhutinh18@gmail.com>
2022-05-12 21:59:17 +0700

    compare-2

---
+++ compare-2.php
@@ -0,0 +1,329 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+
+  <div class="con-compare-intro">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <h3 class="title-lg">Nói chuyện tiếng Anh với người nước ngoài bất kì lúc nào</h3>
+
+          <p class="txt">
+          Một lớp học tại trung tâm tiếng Anh thường rất đông người. Giáo viên gần như không có thời gian để nói chuyện với từng học viên. Bitu giúp bạn nói chuyện 1-1 với người nước ngoài bất kì lúc nào
+          </p>
+
+          <div class="box-btn d-none d-md-block">
+            <button class="btn btn-orange" data-bs-toggle="modal" data-bs-target="#registerNowModal"><span>Đăng ký học thử miễn phí</span></button>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6">
+          <img src="images/compare-intro-2.png" alt="Tiện lợi hơn: Không cần đặt lịch trước với giáo viên." class="img-compare-intro">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /END .con-compare-intro -->
+
+  <div class="con-compare-table">
+    <div class="container">
+      <div class="box-list">
+        <div class="box-head row justify-content-end">
+          <div class="col-4 col-description">
+            <div class="box-inner box-skype">
+              <img src="images/icon_english_center.svg" alt="English Center">
+              <p class="txt">Trung tâm tiếng Anh</p>
+            </div>
+          </div>
+          <div class="col-4 col-description">
+            <div class="box-inner box-bitu">
+              <img src="images/logo_sp.svg" alt="Bitu">
+              <p class="txt">Học qua Bitu</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Mô hình</div>
+            <div class="col-4 col-description">Dạy và học theo lớp học truyền thống từ 10-20 học viên</div>
+            <div class="col-4 col-description">Thực hành nói <br />Tương tác hai chiều</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Nội dung</div>
+            <div class="col-4 col-description">Học theo giáo trình</div>
+            <div class="col-4 col-description">Nói theo chủ đề</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Chú trọng</div>
+            <div class="col-4 col-description">Cả 4 kỹ năng</div>
+            <div class="col-4 col-description">Kỹ năng nói</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Giáo viên</div>
+            <div class="col-4 col-description">Giáo viên Việt Nam và nước ngoài</div>
+            <div class="col-4 col-description">100% moderator nước ngoài</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Thời gian</div>
+            <div class="col-4 col-description">Theo thời khóa biểu</div>
+            <div class="col-4 col-description">Bất kì lúc nào <br />Vào lớp sau 30 giây</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Thiết bị</div>
+            <div class="col-4 col-description">Lớp học truyền thống</div>
+            <div class="col-4 col-description">Smartphone</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Sự tiện lợi</div>
+            <div class="col-4 col-description">
+              <span class="text-primary">Khá bất tiện</span><br />
+              Học viên không được linh hoạt về thời gian học<br />
+              Học viên phải di chuyển đến trung tâm<br />
+              Học viên phải mua, chuẩn bị nhiều giáo trình
+            </div>
+            <div class="col-4 col-description">
+              <span class="text-primary">Ở mức cao nhất</span><br />
+              Học viên chỉ tải duy nhất một ứng dụng về điện thoại<br />
+              Tham gia lúc nào cũng được
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Tính tương tác</div>
+            <div class="col-4 col-description">
+              Tương tác chủ yếu một chiều, thụ động<br />
+              Giáo viên hỏi- Học viên trả lời
+            </div>
+            <div class="col-4 col-description">
+              Tương tác liên tục hai chiều<br />
+              Tăng phản xạ, hình thành khả năng suy nghĩ bằng tiếng Anh
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Tính hiệu quả</div>
+            <div class="col-4 col-description">Học viên chủ yếu tự thực hành với nhau</div>
+            <div class="col-4 col-description">Rất cao nhờ tập trung vào việc thực hành</div>
+          </div>
+        </div>
+
+        <div class="txt-title">Giáo viên/Moderator</div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Quốc tịch</div>
+            <div class="col-4 col-description">Việt Nam và một số quốc gia khác</div>
+            <div class="col-4 col-description">100% giáo viên nước ngoài đến từ các quốc gia chuẩn tiếng Anh</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Năng lượng</div>
+            <div class="col-4 col-description">Nghiêm túc <br />Đứng lớp theo mô típ dạy và học</div>
+            <div class="col-4 col-description">Thân thiện, nhiệt huyết, sôi nổi, tạo không khí lớp cởi mở và vui vẻ nhất</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Quy trình tuyển dụng</div>
+            <div class="col-4 col-description">Lọc hồ sơ, dạy thử</div>
+            <div class="col-4 col-description">Lọc hồ sơ => Phỏng vấn => Dạy thử => Training => Thử việc</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Quy trình đào tạo</div>
+            <div class="col-4 col-description">Giáo viên Việt Nam và nước ngoài</div>
+            <div class="col-4 col-description">100% moderator nước ngoài</div>
+          </div>
+        </div>
+
+        <div class="txt-title">Nội dung</div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Sự đa dạng</div>
+            <div class="col-4 col-description">Thiếu sự đa dạng, chủ yếu sử dụng các giáo trình có sẵn</div>
+            <div class="col-4 col-description">
+              Liên tục cập nhật, đổi mới, bổ sung các chủ đề và từ vựng mới nhất, có tính ứng dụng cao
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Khả năng đáp ứng nhu cầu của học viên</div>
+            <div class="col-4 col-description">Ở mức trung bình do không tập trung chủ yếu vào giao tiếp mà phân tán sự tập trung vào cả 4 kỹ năng</div>
+            <div class="col-4 col-description">
+              Ở mức cao nhất nhờ các bộ chủ đề chuyên sâu như IELTS, TOEIC, Văn phòng, Phỏng vấn, Nhà hàng khách sạn,..., phù hợp mọi nhu cầu của học viên
+            </div>
+          </div>
+        </div>
+
+        <div class="txt-title">Phát âm</div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Sửa lỗi phát âm</div>
+            <div class="col-4 col-description">Chung chung, không hiệu quả</div>
+            <div class="col-4 col-description">Liên tục, chi tiết</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Gợi ý cách trả lời hay</div>
+            <div class="col-4 col-description">
+              Mô hình lớp học đông, giáo viên chủ yếu để học viên nói theo giáo trình, tự luyện tập với nhau<br />
+              Giáo viên không thể theo sát từng học viên<br />
+              Thực hành giữa học viên và giáo viên cực kỳ bị hạn chế
+            </div>
+            <div class="col-4 col-description">
+              Thường xuyên <br />
+              Gợi ý cách trả lời giống với đời sống giao tiếp hàng ngày của người bản ngữ nhất
+            </div>
+          </div>
+        </div>
+
+        <div class="txt-title">Từ vựng</div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Dạng từ vựng</div>
+            <div class="col-4 col-description">
+              Chủ yếu là từ đơn<br />
+              Cụm từ, thành ngữ, tiếng lóng rất ít, không đa dạng hoặc không được cập nhật mới
+            </div>
+            <div class="col-4 col-description">
+              Cả từ đơn, cụm từ, thành ngữ và tiếng lóng <br />
+              Được cập nhật từ vựng mới liên tục theo sự thay đổi trong văn phong giao tiếp của người bản ngữ
+            </div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Tính năng lưu từ mới</div>
+            <div class="col-4 col-description">Không có</div>
+            <div class="col-4 col-description">Có và dễ sử dụng</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Tính năng ôn tập lại từ mới</div>
+            <div class="col-4 col-description">Không có</div>
+            <div class="col-4 col-description">Có và dễ sử dụng</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Tính năng hỗ trợ kiểm tra từ mới</div>
+            <div class="col-4 col-description">Không có</div>
+            <div class="col-4 col-description">Có và dễ sử dụng</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Lưu lại audio cả bài học</div>
+            <div class="col-4 col-description">Không có</div>
+            <div class="col-4 col-description">Tự động lưu trữ</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="row">
+            <div class="col-4 col-title">Đánh giá sau buổi học</div>
+            <div class="col-4 col-description">
+              Ít, hầu như không có <br />
+              Chỉ đánh giá định kỳ bằng các bài kiểm tra
+            </div>
+            <div class="col-4 col-description">Định kỳ, chi tiết</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!-- /END .con-compare-table -->
+
+  <div id="registerNowModal" class="modal register-now-modal" tabindex="-1">
+    <div class="box-bg">
+      <div class="bg">
+        <div class="txt-title d-none d-md-block">Đăng ký học <br />thử miễn phí</div>
+      </div>
+    </div>
+
+    <button type="button" class="btn-close-modal" data-bs-dismiss="modal" aria-label="Close">
+      <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="1.5" y="1.5" width="51" height="51" rx="25.5" stroke="white" stroke-width="3"/><path d="M37 17L17 37" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/><path d="M17 17L37 37" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>
+    </button>
+
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-body">
+          <form class="register-form-common">
+            <div class="form-title d-block d-md-none">Đăng ký lên trình tiếng Anh cùng Bitu</div>
+            <div class="form-group">
+              <div class="form-control-with-pre-icon">
+                <input type="text" class="form-control" placeholder="Họ tên">
+                <svg class="ic" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.4117 6.07523C14.4117 8.52275 12.4493 10.4853 10 10.4853C7.55162 10.4853 5.58838 8.52275 5.58838 6.07523C5.58838 3.6277 7.55162 1.66602 10 1.66602C12.4493 1.66602 14.4117 3.6277 14.4117 6.07523ZM9.99992 18.3327C6.38523 18.3327 3.33325 17.7452 3.33325 15.4785C3.33325 13.211 6.4044 12.6443 9.99992 12.6443C13.6154 12.6443 16.6666 13.2318 16.6666 15.4985C16.6666 17.766 13.5954 18.3327 9.99992 18.3327Z" fill="url(#paint0_linear_1365_4165)" /><defs>  <linearGradient id="paint0_linear_1365_4165" x1="3.33325" y1="1.66602" x2="18.3237" y2="3.57764" gradientUnits="userSpaceOnUse">    <stop offset="1" stop-color="#FF6D03" />    <stop offset="1" stop-color="#FF9A03" />  </linearGradient></defs></svg>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="form-control-with-pre-icon">
+                <input type="text" class="form-control" placeholder="Số điện thoại">
+                <svg class="ic" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.1389 11.984C14.5791 11.8636 14.1363 12.1234 13.7442 12.3504C13.3427 12.5843 12.5791 13.2037 12.1415 13.0452C9.90074 12.1226 7.79328 10.1613 6.88099 7.91161C6.7202 7.46462 7.33671 6.69623 7.56886 6.28995C7.79414 5.89666 8.04865 5.44966 7.93257 4.88572C7.82767 4.37894 6.47085 2.65246 5.99106 2.18034C5.67464 1.86848 5.35049 1.69696 5.01773 1.66924C3.76667 1.61553 2.36943 3.28484 2.12438 3.68419C1.51046 4.53574 1.5139 5.66883 2.1347 7.04274C3.63081 10.7331 9.28939 16.3024 12.9936 17.8547C13.6771 18.1744 14.3022 18.3346 14.8637 18.3346C15.4131 18.3346 15.9024 18.1813 16.3229 17.8772C16.6401 17.6945 18.3779 16.2279 18.3323 14.9432C18.3048 14.6157 18.1337 14.2883 17.8258 13.9712C17.3572 13.487 15.6419 12.0897 15.1389 11.984Z" fill="url(#paint0_linear_934_3667)" /><defs>  <linearGradient id="paint0_linear_934_3667" x1="1.6665" y1="1.66797" x2="20.2374" y2="4.62824" gradientUnits="userSpaceOnUse">    <stop offset="1" stop-color="#FF6D03" />    <stop offset="1" stop-color="#FF9A03" />  </linearGradient></defs></svg>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="form-control-with-pre-icon">
+                <input type="Email" class="form-control" placeholder="Email">
+                <svg class="ic" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.4492 3C15.5667 3 16.6417 3.44167 17.4325 4.23417C18.2242 5.025 18.6667 6.09167 18.6667 7.20833V13.7917C18.6667 16.1167 16.775 18 14.4492 18H6.21667C3.89083 18 2 16.1167 2 13.7917V7.20833C2 4.88333 3.8825 3 6.21667 3H14.4492ZM15.7749 8.44962L15.8416 8.38296C16.0408 8.14129 16.0408 7.79129 15.8324 7.54962C15.7166 7.42546 15.5574 7.34962 15.3916 7.33296C15.2166 7.32379 15.0499 7.38296 14.9241 7.49962L11.1666 10.4996C10.6833 10.9005 9.99077 10.9005 9.49994 10.4996L5.74994 7.49962C5.49077 7.30796 5.13244 7.33296 4.91661 7.55796C4.69161 7.78296 4.66661 8.14129 4.85744 8.39129L4.96661 8.49962L8.75827 11.458C9.22494 11.8246 9.79077 12.0246 10.3833 12.0246C10.9741 12.0246 11.5499 11.8246 12.0158 11.458L15.7749 8.44962Z" fill="url(#paint0_linear_934_3659)" /><defs>  <linearGradient id="paint0_linear_934_3659" x1="2" y1="3" x2="20.4636" y2="6.27019" gradientUnits="userSpaceOnUse">    <stop offset="1" stop-color="#FF6D03" />    <stop offset="1" stop-color="#FF9A03" />  </linearGradient></defs></svg>
+              </div>
+            </div>
+
+            <div class="text-center">
+              <button type="submit" class="btn btn-orange btn-advisory"><span>Đăng ký ngay</span></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /END registerNowModal -->
+
+  <?php include "templates/footer.php"; ?>
+</body>
+
+</html>
